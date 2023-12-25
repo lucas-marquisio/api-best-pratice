4b8ce2101e54b71f14cecd8d402bb2fbe4bc78fe
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-12-25 18:03:46 -0300

    feat: http-server with axios

---
+++ src/infra/http-server.ts
@@ -0,0 +1,37 @@
+import express, { Express, Request, Response } from 'express'
+import { Controller } from '../application/protocos/controller'
+
+export class HttpServer {
+  private server: Express
+
+  constructor () {
+    this.server = express()
+    this.server.use(express.json())
+  }
+
+  get (path: string, controller: Controller) {
+    this.server.get(path, async (request: Request, response: Response) => {
+      const output = await controller.execute({
+        body: request.body,
+        params: request.params
+      })
+
+      return response.status(output.statusCode).json(output.body)
+    })
+  }
+
+  post (path: string, controller: Controller) {
+    this.server.post(path, async (request: Request, response: Response) => {
+      const output = await controller.execute({
+        body: request.body,
+        params: request.params
+      })
+
+      return response.status(output.statusCode).json(output.body)
+    })
+  }
+
+  start (port: number) {
+    this.server.listen(port)
+  }
+}
