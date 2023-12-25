aa5a94fa438c2b69d8b494d32fa5b56aa82772f1
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-12-25 16:06:05 -0300

    feat: PointService - Should checkout and recieve a proof correct

--- src/application/services/point-service.spec.ts
+++ src/application/services/point-service.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test'
-import { Clock, PointService } from './point-service'
+import { Clock, PointRepository, PointService } from './point-service'
 
 class FakeClock implements Clock {
   private time: Date = new Date()
@@ -13,8 +13,32 @@ class FakeClock implements Clock {
   }
 }
 
+class FakePointRepository implements PointRepository {
+  private database: any[] = []
+
+  async save (point: any): Promise<void> {
+    this.database.push(point)
+  }
+
+  async findLast (userid: string): Promise<any> {
+    const proof = this.database.findLast(data => data.userId === userid)
+    return proof
+  }
+
+  async update (point: any): Promise<void> {
+    this.database.forEach(data => {
+      if (data.id === point.id) {
+        data = point
+      }
+    })
+  }
+}
+
 test('should checkin and return proof', async () => {
-  const pointService = new PointService(new FakeClock())
+  const pointService = new PointService(
+    new FakeClock(),
+    new FakePointRepository()
+  )
   const proof = await pointService.checkin('1234')
 
   expect(proof).toHaveProperty('id')
@@ -23,7 +47,7 @@ test('should checkin and return proof', async () => {
 
 test('should return an error if you try to check in before the opening time', async () => {
   const fakeClock = new FakeClock()
-  const pointService = new PointService(fakeClock)
+  const pointService = new PointService(fakeClock, new FakePointRepository())
   fakeClock.setTime(new Date('2023-12-25:07:00:00'))
 
   expect(async () => await pointService.checkin('1234')).toThrow(
@@ -33,9 +57,20 @@ test('should return an error if you try to check in before the opening time', as
 
 test('Should check in and verify if the recorded time matches the one on the proof', async () => {
   const fakeClock = new FakeClock()
-  const pointService = new PointService(fakeClock)
+  const pointService = new PointService(fakeClock, new FakePointRepository())
   fakeClock.setTime(new Date('2023-12-25:08:00:00'))
   const proof = await pointService.checkin('1234')
 
   expect(proof.checkin).toEqual(new Date('2023-12-25:08:00:00'))
+})
+
+test('Should checkout and recieve a proof correct', async () => {
+  const fakeClock = new FakeClock()
+  const pointService = new PointService(fakeClock, new FakePointRepository())
+  fakeClock.setTime(new Date('2023-12-25:08:00:00'))
+  const proofCheckin = await pointService.checkin('1234')
+  fakeClock.setTime(new Date('2023-12-25:14:00:00'))
+  const proofCheckout = await pointService.checkout('1234')
+
+  expect(proofCheckin.id).toEqual(proofCheckout.id)
 })
--- src/application/services/point-service.ts
+++ src/application/services/point-service.ts
@@ -1,8 +1,18 @@
 export interface Clock {
   getTime(): Date
 }
+
+export interface PointRepository {
+  save(point: any): Promise<void>
+  findLast(userid: string): Promise<any>
+  update(point: any): Promise<void>
+}
+
 export class PointService {
-  constructor (private readonly clock: Clock) {}
+  constructor (
+    private readonly clock: Clock,
+    private readonly pointRepository: PointRepository
+  ) {}
 
   async checkin (userId: string) {
     const checkinTime = this.clock.getTime()
@@ -10,9 +20,25 @@ export class PointService {
 
     if (checkinHour < 8) throw new Error('Error, try again after 8 AM.')
 
-    return {
-      id: userId,
-      checkin: this.clock.getTime()
+    const proof = {
+      id: Math.random() * 100,
+      userId,
+      checkin: this.clock.getTime(),
+      checkout: null
     }
+
+    await this.pointRepository.save(proof)
+
+    return proof
+  }
+
+  async checkout (userId: string) {
+    const proof = await this.pointRepository.findLast(userId)
+
+    proof.checkout = this.clock.getTime()
+
+    await this.pointRepository.update(proof)
+
+    return proof
   }
 }
